66948d3697e31b0b211e878d1bc27ca8d4e6bef5
Michael Scott <michaelscott199410@gmail.com>
2025-07-11 16:51:36 +0100

    updated the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,8 +5,8 @@ export default function Footer() {
           &copy; {new Date().getFullYear()} Seanmizzy. All rights reserved.
         </p>
         <div className="mt-2 space-x-4">
-          <a href="https://twitter.com/seanmizzy" className="hover:underline text-blue-400">Twitter</a>
-          <a href="https://github.com/seanmizzy" className="hover:underline text-blue-400">GitHub</a>
+          <a href="https://twitter.com/blackscofieldd" className="hover:underline text-blue-400">Twitter</a>
+          <a href="https://github.com/michaelsturd" className="hover:underline text-blue-400">GitHub</a>
           <a href="/contact" className="hover:underline text-blue-400">Contact</a>
         </div>
       </footer>
